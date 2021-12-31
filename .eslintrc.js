bdbe451c809e6af599da196df49828b265cd98ce
1326241 <978752208@qq.com>
2021-12-31 21:06:54 +0800

    2-3章

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0,
+    'space-before-function-paren': 0, //取消ESLint函数名空号前面加空格的规则校验
+    'eol-last': 0, // 取消最后一个空行的规则校验
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
 }
+}
+
