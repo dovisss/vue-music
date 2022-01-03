83c1eed9824f53eca5afbcb2bd479a250caacec2
1326241 <978752208@qq.com>
2022-01-03 15:46:33 +0800

    2-4

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import Recommend from '@/views/recommend'
+import Search from '@/views/search'
+import Singer from '@/views/singer'
+import TopList from '@/views/top-list'
 const routes = [
+  { // 默认进入recommend页面
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  }, {
+    path: '/singer',
+    component: Singer
+  }, {
+    path: '/top-list',
+    component: TopList
+  }, {
+    path: '/search',
+    component: Search
+  }
 ]
 
 const router = createRouter({
